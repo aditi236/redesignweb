ba0dbef4c7c0ef71fd3431cbdc7736f5e67bb49b
Aditi Singh <113187245+aditi236@users.noreply.github.com>
2022-09-17 16:22:01 +0400

    Add files via upload

---
+++ Redesigned Website/code.js
@@ -0,0 +1,23 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156068923-1">
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-156068923-1');
+
+ type="text/javascript" src="swfobject.js"
+ type="text/javascript"
+//[CDATA[
+if (swfobject.hasFlashPlayerVersion("6.0.0")) {
+swfobject.createCSS("html", "height:100%;");
+swfobject.createCSS("body", "overflow:hidden; height:100%;");
+swfobject.createCSS("#myContent", "margin:0; width:100%;");	}
+var flashvars = false;
+var params = { bgcolor: "#000000", wmode: "transparent", allowScriptAccess: "sameDomain", allowFullScreen: "true"};
+var attributes = false;
+swfobject.embedSWF("entrance.swf", "myContent", "100%", "100%", "6.0.0", false, flashvars, params, attributes);
+//]];
+
+<script type="text/javascript" src="date.js">
+</script>
